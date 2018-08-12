3a606828ea5ec55d05e15a8a9a03948d38f7d113
Siavash Outadi <siavash.outadi@gmail.com>
2018-08-12 13:22:21 +0200

    Add dashboard module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PagenotfoundModule } from './pagenotfound/pagenotfound.module';
+import { DashboardModule } from './dashboard/dashboard.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { PagenotfoundModule } from './pagenotfound/pagenotfound.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    PagenotfoundModule
+    PagenotfoundModule,
+    DashboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
