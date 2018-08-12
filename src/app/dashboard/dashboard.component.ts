f24514b6f6d5e6e16db679c28c0ae777b0794fef
Siavash Outadi <siavash.outadi@gmail.com>
2018-08-12 13:36:33 +0200

    Add a navbar and a side bar to the dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,76 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+import { MediaMatcher } from '@angular/cdk/layout';
 
 @Component({
   selector: 'siaout-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnDestroy {
+  mobileQuery: MediaQueryList;
+  sideNavItems = [
+    {
+      "title": "Settings",
+      "icon": "settings",
+      "children": [
+        {
+          "title": "Edit Menu",
+          "route": "dashboard/menu/edit"
+        }
+      ]
+    },
+    {
+      "title": "Dashboard",
+      "route": "/overview",
+      "icon": "dashboard"
+    },
+    {
+      "title": "Products",
+      "icon": "work",
+      "children": [
+        {
+          "title": "Add",
+          "route": "products/add"
+        },
+        {
+          "title": "Edit",
+          "route": "products/edit"
+        },
+        {
+          "title": "Delete",
+          "route": "products/delete"
+        }
+      ]
+    },
+    {
+      "title": "Users",
+      "icon": "face",
+      "children": [
+        {
+          "title": "Add",
+          "route": "users/add"
+        },
+        {
+          "title": "Edit",
+          "route": "users/edit"
+        },
+        {
+          "title": "Delete",
+          "route": "users/delete"
+        }
+      ]
+    }
+  ];
+  private _mobileQueryListener: () => void;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, router: Router) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
   }
 
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
 }
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,12 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
+import {
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatListModule,
+    MatExpansionModule,
+    MatRippleModule
+} from '@angular/material';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [DashboardComponent],
-  exports: [DashboardComponent]
+    imports: [
+        MatSidenavModule,
+        MatButtonModule,
+        MatIconModule,
+        MatToolbarModule,
+        MatRippleModule,
+        MatListModule,
+        MatExpansionModule,
+        RouterModule,
+        CommonModule
+    ],
+    exports: [DashboardComponent],
+    declarations: [
+        DashboardComponent
+    ]
 })
 export class DashboardModule { }
